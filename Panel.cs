f02efa362306dedd686aa797ab170026e3749e8d
willychang21 <38349902+willychang21@users.noreply.github.com>
2018-05-20 01:22:36 +0800

    Show Panel

---
+++ Panel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour {
+
+    public void OnBtnShowClick()
+    {
+        Debug.Log("Panel關閉");
+        UIManager.checkopen = true;
+        UIManager.Instance.ClosePanel("Panel");
+    }
+}
