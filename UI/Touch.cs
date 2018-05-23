37c8561930aeb03398be09265a941992b0befa44
willychang21 <38349902+willychang21@users.noreply.github.com>
2018-05-24 00:07:38 +0800

    Create Touch.cs

---
+++ UI/Touch.cs
@@ -0,0 +1,46 @@
+
+using Mapbox.Examples;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Utilities;
+using Mapbox.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class touchdis文開 : MonoBehaviour 
+{
+
+
+
+    
+
+    void Update()
+        {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+               
+                if (hit.transform != null)
+                {
+                       
+                        
+
+                        Debug.Log("Panel文開打開");
+                        UIManager.Instance.ShowPanel("Panel文開");
+                    
+                }
+                
+                
+
+
+            }
+        }
+        
+        
+    }
+}
