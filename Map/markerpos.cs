c052a92a23c971f3541e63cd6752f244789b5366
willychang21 <38349902+willychang21@users.noreply.github.com>
2018-06-15 16:46:30 +0800

    Create markerpos.cs

---
+++ Map/markerpos.cs
@@ -0,0 +1,23 @@
+using Mapbox.Unity.MeshGeneration.Data;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class markerpos : MonoBehaviour {
+
+ 
+    [SerializeField]
+    private Transform _landmark;
+
+
+    Vector3 landmarkpos  = PoiMarkerHelper.screenPos;
+
+    public void Update()
+    {
+        Debug.Log("markpos =" + landmarkpos);
+        _landmark.transform.position = landmarkpos;
+    }
+
+
+}
+
