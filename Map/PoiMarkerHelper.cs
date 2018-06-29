70277c63ae15cc758cc7072da683117617de6642
willychang21 <38349902+willychang21@users.noreply.github.com>
2018-06-29 17:41:47 +0800

    Update PoiMarkerHelper.cs

--- Map/PoiMarkerHelper.cs
+++ Map/PoiMarkerHelper.cs
@@ -7,59 +7,44 @@ using Mapbox.Unity.MeshGeneration.Modifiers;
 
 public class PoiMarkerHelper : MonoBehaviour, IFeaturePropertySettable
 {
-
-    Dictionary<string, object> _props;
-    private markerpos _marker;
-    private GameObject mapcam;
-    private Camera cam;
-    public static Vector3 screenPos;
-
-    void Start()
-    {
-        FindCamera();
-    }
-
-    public void Set(Dictionary<string, object> props)
-    {
-        _props = props;
+	Dictionary<string, object> _props;
+	private GameObject mapcam;
+	private GameObject waypoint;
+	private Camera cam;
+	private Transform waypointrans;
+	private Vector3 screenPoint;
 
 
-    }
-    void FindCamera()
-    {
-        mapcam = GameObject.Find("MapCamera");
-        cam = mapcam.GetComponent<Camera>();
+	void Start()
+	{
+		FindObject();
+	}
 
-    }
+	public void Set(Dictionary<string, object> props)
+	{
+		_props = props;
 
 
+	}
+	void FindObject()
+	{
+		mapcam = GameObject.Find("MapCamera");
+		waypoint = GameObject.Find("Waypoint2");
+		cam = mapcam.GetComponent<Camera>();
+		waypointrans = waypoint.GetComponent<Transform>();
 
-    public void OnMouseUpAsButton()
-    {
+	}
 
-        Debug.Log("mappos :"+gameObject.GetComponent<Transform>().position);
-        Debug.Log("screenpos :"+cam.WorldToScreenPoint(gameObject.GetComponent<Transform>().position));
-        screenPos = cam.WorldToScreenPoint(gameObject.GetComponent<Transform>().position);
+	public void OnMouseUpAsButton()
+	{
 
-        if (marker.markercheck >= 0)
-        {
-            Canvas canvas;
-            var go = new GameObject("InteractiveSelectionCanvas", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster));
-            canvas = go.GetComponent<Canvas>();
-            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-
-            var sel = Instantiate(Resources.Load<GameObject>("selector"));
-            sel.transform.SetParent(canvas.transform);
-            _marker = sel.GetComponent<markerpos>();
-            marker.markercheck++;
-        }
-        
-        /*
+		Debug.Log("mappos :" + gameObject.GetComponent<Transform>().position);						
         foreach (var prop in _props)
         {
             Debug.Log(prop.Key + ":" + prop.Value);
-        }*/
-    }
+        }
+		waypointrans.position = gameObject.GetComponent<Transform>().position;
+	}
 
 
 
